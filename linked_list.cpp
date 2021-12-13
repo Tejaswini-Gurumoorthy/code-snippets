3bbc2a7e279b23cfc77365381edd3041470572e6
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2021-12-13 18:13:53 +0530

    Update linked_list.cpp

--- linked_list.cpp
+++ linked_list.cpp
@@ -28,7 +28,7 @@ class LinkedList
     {
         head=NULL;
     }
-    void insert(int x)
+    void insertAtEnd(int x)
     {
         Node *newNode = new Node(x);
         if(head==NULL)
@@ -42,6 +42,62 @@ class LinkedList
             temp= temp->next;
         }
         temp->next =newNode;
+
+    }
+    void insertAtBeg(int x)
+    {
+        Node *newNode = new Node(x);
+        Node *temp= head;
+        head= newNode;
+        newNode->next= temp;
+    }
+    void insertAtPos(int x, int pos)
+    {
+        Node *newNode = new Node(x);
+        Node *temp = head;
+        Node *tempPrev= NULL;
+        if(head==NULL)
+        {
+            cout<<"NO ELEMENTS FOUND !"<<endl;
+            return;
+        }
+        for(int i=1;i<pos;i++)
+        {
+            tempPrev= temp;
+            temp= temp->next;
+        }
+        tempPrev->next=newNode;
+        newNode->next= temp;
+    }
+    void deleteAtPos(int pos)
+    {
+        Node *temp = head;
+        Node *tempPrev= NULL;
+        if(head==NULL)
+        {
+            cout<<"NO ELEMENTS FOUND !"<<endl;
+            return;
+        }
+        for(int i=1;i<pos;i++)
+        {
+            tempPrev= temp;
+            temp= temp->next;
+        }
+        tempPrev->next= temp->next;
+        delete temp;
+
+    }
+    void deleteAtEnd()
+    {
+        Node *temp= head;
+        Node *tempPrev= NULL;
+        while(temp->next!=NULL)
+        {
+            tempPrev=temp;
+            temp= temp->next;
+        }
+        tempPrev->next= NULL;
+        delete temp;
 
     }
 
@@ -65,11 +121,17 @@ class LinkedList
 int main()
 {
     LinkedList l;
+    l.insertAtEnd(1);
+    l.insertAtEnd(2);
+    l.insertAtEnd(6);
+    l.insertAtEnd(4);
+    l.insertAtEnd(5);
     l.print();
-    l.insert(1);
-    l.insert(2);
-    l.insert(3);
-    l.insert(4);
-    l.insert(5);
+    l.deleteAtPos(3);
+    l.print();
+    l.insertAtPos(3,3);
+    l.print();
+    l.insertAtBeg(0);
+    l.deleteAtEnd();
     l.print();
 }
