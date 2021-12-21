3a841513848d3e77d62665358592786befdb4a4e
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2021-12-21 08:40:26 +0530

    Update single_linked_list.cpp

--- single_linked_list.cpp
+++ single_linked_list.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 class Node
 {
-    public:
+public:
     int data;
     Node *next;
 
-    
     Node()
     {
-        data=0;
-        next=NULL;
+        data = 0;
+        next = NULL;
     }
     Node(int x)
     {
@@ -23,115 +22,192 @@ class Node
 class LinkedList
 {
     Node *head;
-    public:
+
+public:
     LinkedList()
     {
-        head=NULL;
+        head = NULL;
     }
     void insertAtEnd(int x)
     {
         Node *newNode = new Node(x);
-        if(head==NULL)
+        if (head == NULL)
         {
-            head=newNode;
+            head = newNode;
             return;
         }
-        Node *temp= head;
-        while(temp->next!=NULL)
+        Node *temp = head;
+        while (temp->next != NULL)
         {
-            temp= temp->next;
+            temp = temp->next;
         }
-        temp->next =newNode;
-
+        temp->next = newNode;
     }
     void insertAtBeg(int x)
     {
         Node *newNode = new Node(x);
-        Node *temp= head;
-        head= newNode;
-        newNode->next= temp;
+        Node *temp = head;
+        head = newNode;
+        newNode->next = temp;
+    }
+
+    void insertBeforeEle(int x, int ele)
+    {
+        Node *newNode = new Node(ele);
+        Node *temp = head;
+        Node *tempPrev = NULL;
+        if (head == NULL)
+        {
+            cout << "NO ELEMENTS FOUND !" << endl;
+            return;
+        }
+        while (temp != NULL)
+        {
+            if ((temp->data == x) && (temp == head))
+            {
+                insertAtBeg(ele);
+            }
+            else if (temp->data == x)
+            {
+                tempPrev->next = newNode;
+                newNode->next = temp;
+                break;
+            }
+            else
+            {
+                tempPrev = temp;
+                temp = temp->next;
+            }
+        }
+    }
+
+    void insertAfterEle(int x, int ele)
+    {
+        Node *newNode = new Node(ele);
+        Node *temp = head;
+        Node *tempPrev = temp;
+        if (head == NULL)
+        {
+            cout << "NO ELEMENTS FOUND !" << endl;
+            return;
+        }
+        while (temp != NULL)
+        {
+            if ((temp->data == x) && (temp == head))
+            {
+                insertAtBeg(ele);
+            }
+            else if (tempPrev->data == x)
+            {
+                tempPrev->next = newNode;
+                newNode->next = temp;
+                break;
+            }
+            else
+            {
+                tempPrev = temp;
+                temp = temp->next;
+            }
+        }
     }
     void insertAtPos(int x, int pos)
     {
         Node *newNode = new Node(x);
         Node *temp = head;
-        Node *tempPrev= NULL;
-        if(head==NULL)
+        Node *tempPrev = NULL;
+        if (head == NULL)
         {
-            cout<<"NO ELEMENTS FOUND !"<<endl;
+            cout << "NO ELEMENTS FOUND !" << endl;
             return;
         }
-        for(int i=1;i<pos;i++)
+        for (int i = 1; i < pos; i++)
         {
-            tempPrev= temp;
-            temp= temp->next;
+            tempPrev = temp;
+            temp = temp->next;
         }
-        tempPrev->next=newNode;
-        newNode->next= temp;
+        tempPrev->next = newNode;
+        newNode->next = temp;
     }
+
     void deleteAtPos(int pos)
     {
         Node *temp = head;
-        Node *tempPrev= NULL;
-        if(head==NULL)
+        Node *tempPrev = NULL;
+        if (head == NULL)
         {
-            cout<<"NO ELEMENTS FOUND !"<<endl;
+            cout << "NO ELEMENTS FOUND !" << endl;
             return;
         }
-        for(int i=1;i<pos;i++)
+        for (int i = 1; i < pos; i++)
         {
-            tempPrev= temp;
-            temp= temp->next;
+            tempPrev = temp;
+            temp = temp->next;
         }
-        tempPrev->next= temp->next;
+        tempPrev->next = temp->next;
         delete temp;
+    }
+    void deleteEle(int ele)
+    {
+        Node *temp = head;
+        Node *tempPrev = temp;
+        if (head == NULL)
+        {
+            cout << "NO ELEMENTS FOUND !" << endl;
+            return;
+        }
+        while (temp != NULL)
+        {
+            if ((temp->data == ele) && (temp == head))
+            {
+                head = temp->next;
+                delete temp;
+            }
 
+            else if (temp->data == ele)
+            {
+                tempPrev->next = temp->next;
+                delete temp;
+                break;
+            }
+            else
+            {
+                tempPrev = temp;
+                temp = temp->next;
+            }
+        }
     }
     void deleteAtEnd()
     {
-        Node *temp= head;
-        Node *tempPrev= NULL;
-        while(temp->next!=NULL)
+        Node *temp = head;
+        Node *tempPrev = NULL;
+        while (temp->next != NULL)
         {
-            tempPrev=temp;
-            temp= temp->next;
+            tempPrev = temp;
+            temp = temp->next;
         }
-        tempPrev->next= NULL;
+        tempPrev->next = NULL;
         delete temp;
-
     }
 
     void print()
     {
-        Node *temp= head;
-        if(head==NULL)
+        Node *temp = head;
+        if (head == NULL)
         {
-            cout<<"The list is empty"<<endl;
+            cout << "The list is empty" << endl;
             return;
         }
-        while(temp!=NULL)
+        while (temp != NULL)
         {
-            cout<<temp->data<<" -> ";
-            temp= temp->next;
-        }
-        cout<<endl;
+            cout << temp->data;
 
+            temp = temp->next;
+            if (temp == NULL)
+            {
+                break;
+            }
+            cout << "->";
+        }
+        cout << endl;
     }
 };
-int main()
-{
-    LinkedList l;
-    l.insertAtEnd(1);
-    l.insertAtEnd(2);
-    l.insertAtEnd(6);
-    l.insertAtEnd(4);
-    l.insertAtEnd(5);
-    l.print();
-    l.deleteAtPos(3);
-    l.print();
-    l.insertAtPos(3,3);
-    l.print();
-    l.insertAtBeg(0);
-    l.deleteAtEnd();
-    l.print();
-}
