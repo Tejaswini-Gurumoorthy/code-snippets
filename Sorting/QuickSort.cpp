9a9121bf1d4753113931cf20ab7daa8ff9f819ba
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2022-05-09 20:55:17 +0530

    wrong

--- Sorting/QuickSort.cpp
+++ Sorting/QuickSort.cpp
@@ -1,71 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void swapit(int *a, int*b)
+void swapit(int *a, int *b)
 {
-    int temp= *a;
-    *a= *b;
-    *b= temp;
-
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-int partition(int a[],int start, int end)
+int partition(int a[], int start, int end)
 {
-    //cout<<"I came here to partition"<<endl;
-    int pivot= a[start];
-    //int piv_pos=start;
-    int s= start, e= end;
-    while(s<=e)
+    // cout<<"I came here to partition"<<endl;
+    int pivot = a[start];
+    // int piv_pos=start;
+    int s = start, e = end;
+    while (s < e)
     {
-        while(s<=pivot)
+        while (s <= pivot)
         {
             s++;
         }
-        while(e>pivot)
+        while (e > pivot)
         {
             e--;
         }
-        if(s<e)
+        if (s < e)
         {
-            swapit(&a[s],&a[e]);
+            swapit(&a[s], &a[e]);
         }
     }
-    swapit(&a[start],&a[e]);
+    swapit(&a[start], &a[e]);
     return e;
-
-
 }
-void quicksort(int a[],int start,int end)
+void quicksort(int a[], int start, int end)
 {
-    //cout<<"Came to quicksort"<<endl;
-    int loc= partition(a,start,end);
-    quicksort(a,start,loc-1);
-    quicksort(a,loc+1,end);
-
-}
-void printarray(int a[],int n)
-{
-    for(int i=0;i<n;i++)
+    // cout<<"Came to quicksort"<<endl;
+    if (start < end)
     {
-        cout<<a[i]<<" ";
+        int loc = partition(a, start, end);
+        quicksort(a, start, loc - 1);
+        quicksort(a, loc + 1, end);
     }
-    cout<<endl;
+}
+void printarray(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
 {
     int n;
-    cout<<"Enter number of elements : "<<endl;
-    cin>>n;
+    cout << "Enter number of elements : " << endl;
+    cin >> n;
     int a[n];
-    cout<<"Enter elements : "<<endl;
-    for(int i=0;i<n;i++)
+    cout << "Enter elements : " << endl;
+    for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
-    quicksort(a,0,n-1);
-    cout<<"sorted array : "<<endl;
-    printarray(a,n);
-    
-
+    quicksort(a, 0, n - 1);
+    cout << "sorted array : " << endl;
+    printarray(a, n);
 }
